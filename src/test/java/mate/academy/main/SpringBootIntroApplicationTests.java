b4cfe7d51fc1eb94636147717492509cb0ea36e3
ReduxR <177857999+ReduxR@users.noreply.github.com>
2026-07-08 18:50:52 +0200

    initial commit

---
+++ src/test/java/mate/academy/main/SpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
