092535679e8e38d10a7245f89e9779cfc2329318
ReduxR <177857999+ReduxR@users.noreply.github.com>
2026-08-03 21:05:31 +0200

    refactor: remove redundant methods in UserRepository

--- src/main/java/com/reduxr/repository/UserRepository.java
+++ src/main/java/com/reduxr/repository/UserRepository.java
@@ -1,13 +1,8 @@
 package com.reduxr.repository;
 
 import com.reduxr.model.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    
-    boolean existsUserByEmail(String email);
-    
     boolean existsByEmail(String email);
 }
