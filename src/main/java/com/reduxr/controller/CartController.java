a4ddefc8409f29219f4ef0c3e34e1b46c4b6349e
ReduxR <177857999+ReduxR@users.noreply.github.com>
2026-08-18 13:43:48 +0200

    docs: add swagger documentation for cart controller

--- src/main/java/com/reduxr/controller/CartController.java
+++ src/main/java/com/reduxr/controller/CartController.java
@@ -5,9 +5,12 @@ import com.reduxr.dto.cart.CreateCartItemRequestDto;
 import com.reduxr.dto.cart.ShoppingCartDto;
 import com.reduxr.dto.cart.UpdateCartItemRequestDto;
 import com.reduxr.service.ShoppingCartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,23 +21,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Shopping cart management", description = "User endpoints for managing shopping cart")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/cart")
 public class CartController {
     private final ShoppingCartService shoppingCartService;
     
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @Operation(summary = "Get shopping cart", description = "Get shopping cart " 
+            + "of currently logged user")
     @GetMapping
     public ShoppingCartDto getShoppingCart() {
         return shoppingCartService.getShoppingCart();
     }
     
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @Operation(summary = "Add cart item", description = "Create new cart item in shopping cart")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CartItemDto addItemToCart(@RequestBody @Valid CreateCartItemRequestDto requestDto) {
         return shoppingCartService.saveCartItem(requestDto);
     }
     
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @Operation(summary = "Update item quantity", description = "Update cart item quantity by id")
     @PutMapping("/items/{cartItemId}")
     public CartItemDto updateItemQuantityInCart(
             @PathVariable Long cartItemId, 
@@ -42,6 +53,8 @@ public class CartController {
         return shoppingCartService.updateCartItem(cartItemId, requestDto);
     }
     
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @Operation(summary = "Delete item", description = "Delete item from shopping cart by id")
     @DeleteMapping("/items/{cartItemId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteItemFromCart(@PathVariable Long cartItemId) {
