1f12b25252b2539b6d891cd2cb1314e7097f75dc
ReduxR <177857999+ReduxR@users.noreply.github.com>
2026-09-16 16:27:11 +0200

    fix: add EntityGraph for findAll method with specification

--- src/main/java/com/reduxr/repository/BookRepository.java
+++ src/main/java/com/reduxr/repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.reduxr.repository;
 import com.reduxr.model.Book;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -10,6 +13,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(Specification<Book> specification, Pageable pageable);
+    
     @Query(value = """ 
             SELECT b 
             FROM Book b
